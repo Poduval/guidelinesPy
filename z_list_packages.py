86f5b5cd9a3a79a98842ea9766bfbfc888c657f3
Rakesh Poduval <poduval@IAZI.LOCAL>
2019-01-01 03:40:43 +0100

    added first cut of basic python features

---
+++ z_list_packages.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 30 09:56:39 2018
+
+@author: poduval
+"""
+
+import pip
+installed_packages = pip.get_installed_distributions()
+installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
+     for i in installed_packages])
+print(installed_packages_list)
+
